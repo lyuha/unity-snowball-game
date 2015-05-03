6e39cd2aa857da0a5a6f8c8c11b70bbc9be979e0
minor5pm <minor5pm@gmail.com>
2015-05-03 19:12:29 +0900

    정성우 개발 버전

---
+++ Assets/BallCollision.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallCollision : MonoBehaviour {
+	public AudioClip hitAudio;
+	AudioSource audioSource;
+	GameObject sphere;
+
+	void Start() {
+		audioSource = GetComponent<AudioSource>();
+		sphere = transform.FindChild("Sphere").gameObject;
+
+	}
+
+	void OnCollisionEnter (Collision other) {
+		switch(other.gameObject.tag) {
+		case "Terrain":
+			Hit ();
+			break;
+		case "Hittable":
+			GameObject.Destroy(other.gameObject);
+			Hit ();
+			break;
+		}
+	}
+
+	void Hit() {
+		audioSource.clip = hitAudio;
+		audioSource.Play();
+		print(sphere.transform.position);
+		print(audioSource.minDistance);
+		print(audioSource.maxDistance);
+		GameObject.Destroy(sphere);
+	}
+}
