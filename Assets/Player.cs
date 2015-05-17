e4f3f55bc2d76088deb9a17cc10c56ef0ea64d00
minor5pm <minor5pm@gmail.com>
2015-05-17 16:45:51 +0900

    Imported map

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -46,21 +46,23 @@ public class Player : MonoBehaviour, IDamageable {
 	}
 
 	public void Move(Vector3 movement){
-		if(movement.magnitude != 0f) {
-			if(movedDistance >= weightingDistance) {
-				movedDistance -= weightingDistance;
-				weight += 1;
-			}
+		if(movedDistance >= weightingDistance) {
+			movedDistance -= weightingDistance;
+			weight += 1;
+		}
+		
+		//rigidbody.MovePosition(transform.position + transform.TransformVector(movement));
+		characterController.Move(transform.TransformVector(movement));
 
-			//rigidbody.MovePosition(transform.position + transform.TransformVector(movement));
-			characterController.Move(transform.TransformVector(movement));
+		// movement
+		movement.y = 0f;
+		movedDistance += movement.magnitude;
+
+		if(movement.magnitude != 0f) {
 			if(!audioSource.isPlaying) {
 				audioSource.clip = walkAudioClip;
 				audioSource.Play();
 			}
-
-			movement.y = 0f;
-			movedDistance += movement.magnitude;
 		}
 	}
 
