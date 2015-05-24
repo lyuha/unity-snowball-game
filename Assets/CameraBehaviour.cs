339994439cd7716423bac4e0c6d4d156fabc14fc
minor5pm <minor5pm@gmail.com>
2015-05-24 17:59:11 +0900

    킬캠 구현

---
+++ Assets/CameraBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+	Camera rootCam;
+
+	void Start() {
+		rootCam = transform.Find("/RootCamera").GetComponent<Camera>() as Camera;
+	}
+
+	void OnDestroy() {
+		rootCam.CopyFrom(transform.GetComponent<Camera>() as Camera);
+		rootCam.enabled = true;
+	}
+}
