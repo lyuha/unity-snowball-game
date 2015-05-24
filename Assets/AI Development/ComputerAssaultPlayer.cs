2c947a250875213272baed8c3168e5ec131a3803
Ardie H. Hwang <iam@ardiefox.me>
2015-05-24 17:51:45 +0900

    ComputerAssaultPlayer 구현 및 Player에 메서드 추가

--- Assets/AI Development/ComputerAssaultPlayer.cs
+++ Assets/AI Development/ComputerAssaultPlayer.cs
@@ -1,5 +1,62 @@
-﻿public class ComputerAssaultPlayer : Player, IAssaultable {
-	void IAssaultable.assault(){
+﻿using UnityEngine;
 
+
+public class ComputerAssaultPlayer : Player, IAssaultable {
+
+	public float fieldOfView = 60f;
+
+	private bool shouldFireSnowball = false;
+
+	private ComputerPlayerApproachLimit limitChecker;
+	private NavMeshAgent router;
+	private GameObject humanPlayer;
+
+	void Awake() {
+		limitChecker = GetComponentInParent<ComputerPlayerApproachLimit>();
+		router = GetComponent<NavMeshAgent>();
+		humanPlayer = GameObject.FindGameObjectWithTag("player");
+	}
+
+	public void assault() {
+		shouldFireSnowball = false;
+		
+		// Create a vector from the enemy to the player and store the angle between it and forward.
+		Vector3 direction = humanPlayer.transform.position - transform.position;
+		float angle = Vector3.Angle(direction, transform.forward);
+		
+		// If the angle between forward and where the player is, is less than half the angle of view...
+		if(angle < fieldOfView / 2) {
+			RaycastHit hit;
+			
+			// ... and if a raycast towards the player hits something...
+			if(Physics.Raycast(transform.position + transform.up, direction.normalized, out hit)) {
+				// ... and if the raycast hits the player...
+				if(hit.collider.tag == "player") {
+					// ... the player is in sight.
+					shouldFireSnowball = true;
+					
+					// Set the last global sighting is the players current position.
+					//lastPlayerSighting.position = player.transform.position;
+				}
+			}
+		}
+
+	}
+
+	new void Update() {
+		base.Update();
+
+		assault();
+
+		if(limitChecker.shouldStopMoving == false) {
+			// move
+		}
+		if(shouldFireSnowball == true) {
+			float angle = FindAngle(transform.forward, humanPlayer.transform, transform.up);
+			// lookat
+			// set shoothole "horizontal".
+
+			Shoot(.7f);
+		}
 	}
 }
---
+++ Assets/AI Development/ComputerPlayerApproachLimit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComputerPlayerApproachLimit : MonoBehaviour {
+
+	public bool shouldStopMoving = false;
+
+	//private GameObject humanPlayer;
+
+	//void Awake() {
+	//	humanPlayer = GameObject.FindGameObjectWithTag("player");
+	//}
+
+	void OnTriggerEnter(Collider sphereCollider) {
+		if(sphereCollider.tag == "player")
+			shouldStopMoving = true;
+	}
+
+	void OnTriggerExit(Collider sphereCollider) {
+		if(sphereCollider.tag == "player")
+			shouldStopMoving = false;
+	}
+}
